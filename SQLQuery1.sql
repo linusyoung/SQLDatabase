d02170c029ddcb264d9dbbcd288f1e233b9827c3
Linus Yang <linusyoung@outlook.com>
2017-11-25 20:56:07 +1000

    add section 2 codes

--- SQLQuery1.sql
+++ SQLQuery1.sql
@@ -1,16 +1,1 @@
-﻿/*
-	Section 2 codes - tables
-*/
-
-CREATE TABLE SalesLT.Courier
-(
-CourierID int NOT NULL,
-CourierCode char(3) NOT NULL,
-CourierName nvarchar(50) NOT NULL,
-PRIMARY KEY (CourierID, CourierCode)
-)
-
-ALTER TABLE SalesLT.Courier
-ADD Telephone varchar(15) NULL, Email varchar(25) NULL;
-
-DROP TABLE SalesLT.Courier;
+﻿
---
+++ SqlQuery_1.sql
@@ -0,0 +1,16 @@
+﻿/*
+	Section 2 codes - tables
+*/
+
+CREATE TABLE SalesLT.Courier
+(
+CourierID int NOT NULL,
+CourierCode char(3) NOT NULL,
+CourierName nvarchar(50) NOT NULL,
+PRIMARY KEY (CourierID, CourierCode)
+)
+
+ALTER TABLE SalesLT.Courier
+ADD Telephone varchar(15) NULL, Email varchar(25) NULL;
+
+DROP TABLE SalesLT.Courier;
